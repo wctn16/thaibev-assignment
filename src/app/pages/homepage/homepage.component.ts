1050b42b7e7d4ba48c057fab22bb96880f2600aa
Wichittranut <wichittranut@gmail.com>
2026-05-15 16:21:55 +0700

    update: enhance homepage layout with user profile and comments section

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,17 +1,37 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { DatePipe, NgIf, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, NgIf, NgFor, FormsModule],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+  
+  initial: string = '';
+  postData:any = {
+    username: 'Cat',
+    date : new Date(),
+    image: 'https://images.unsplash.com/photo-1472437774355-71ab6752b434?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  }
+
+  commentsData: { username: string; text: string }[] = [];
+
+  newComment: { username: string; text: string } = { username: 'Blend 285', text: '' };
+
+  addComment(): void {
+    console.log('Adding comment:', this.newComment);
+    
+    if (this.newComment.text.trim()) {
+      // this.commentsData.push({ username: 'Blend 285', text: this.newComment.text.trim() });
+      this.newComment.text = '';
+    }
+  }
 
   ngOnInit(): void {
-    
+    this.initial = this.postData.username.charAt(0).toUpperCase();
   }
 }
